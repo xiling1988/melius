62b23fbe169518f39f8b1c6dd8b3c4e0c67d98c4
hans schilling <hasp1988@gmail.com>
2024-05-07 11:42:57 +0400

    edited sanity config

--- sanity/sanity.cli.js
+++ sanity/sanity.cli.js
@@ -3,6 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: '6uvvqndd',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
